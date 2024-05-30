62baf02b838d6abf82e1d023fea99cd17871270e
Dezzzire <Dezzz1re@yandex.ru>
2024-05-30 20:44:47 +0300

    Add files via upload

---
+++ Task3/Task3/Task3.cpp
@@ -0,0 +1,35 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main(int argc, char* argv[]) {
+    setlocale(LC_ALL, "ru_RU.UTF-8"); // Установим текущую локаль
+
+    if (argc < 2) {
+        printf("Usage: %s <file1> <file2> ... <fileN>\n", argv[0]);
+        return 1;
+    }
+
+    for (int i = 1; i < argc; i++) {
+        FILE* file;
+        if (fopen_s(&file, argv[i], "r") != 0) {
+            printf("Не получается открыть текстовый файл: %s\n", argv[i]);
+            continue;
+        }
+
+        printf("Открытый текстовый файл: %s\n", argv[i]);
+        printf("=====================================\n");
+
+        char line[256];
+        int lineNumber = 1;
+        while (fgets(line, sizeof(line), file) != NULL) {
+            printf("%d: %s", lineNumber, line);
+            lineNumber++;
+        }
+
+        printf("=====================================\n");
+
+        fclose(file);
+    }
+
+    return 0;
+}
